12a1db2b4ecc715aadd9b0c34fbf508ca0f4f6ce
ZX <wszxdhr@126.com>
2026-01-05 03:09:42 +0000

    feat: 添加 Document 和 Page 模块的抽象类

---
+++ src/Page/index.ts
@@ -0,0 +1,21 @@
+import { Number2 } from '@hamster-note/types'
+
+export enum RenderViews {
+  THUMBNAIL = 'thumbnail',
+  TEXT = 'text'
+}
+
+export interface RenderOptions {
+  views: RenderViews[]
+  scale: number
+}
+
+export abstract class HamsterPage {
+  abstract render(
+    container: HTMLDivElement,
+    options?: RenderOptions
+  ): Promise<void>
+  abstract getNumber(): number
+  abstract getSize(scale: number): Number2
+  abstract getPureText(): string
+}
